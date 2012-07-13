b7260aef7cd5536635c8e9e934512bebb60548e6
Frank Cox <frank.l.cox@gmail.com>
2012-07-12 23:44:03 -0700

    moved json class code to a single file

--- mazeBotJSON.h
+++ mazeBotJSON.h
@@ -26,8 +26,49 @@ namespace us_munging_cpp {
     
     };
 
+    
+    void MazeBotJSON::jsonMap(const MazeBot &mb) {
+        std::ofstream jfile;
+        jfile.open("game.json", std::ios::out);
+
+        if (jfile.is_open()) {
+            jfile << "{ \"start\":[" << mb.start.x << "," << mb.start.y << "," << mb.start.z << "]," << std::endl;
+            jfile << "\"end\":[" << mb.end.x << "," << mb.end.y << "," << mb.end.z << "]," << std::endl;
+            jfile << "\"mb_height\":" << mb.mb_height << "," << std::endl;
+            jfile << "\"mb_width\":" << mb.mb_width << "," << std::endl;
+            jfile << "\"mb_depth\":" << mb.mb_depth << "," << std::endl;
+
+            jfile << "\"map\":[" << std::endl;
+            for (int d = 0; d < mb.mb_depth; d++) {
+                jfile << "  [" << std::endl;
+                for (int h = 0; h < mb.mb_height; h++) {
+                    jfile << "    [ ";
+                    for (int w = 0; w < mb.mb_width; w++) {
+                        jfile << "[{" << "\"solid\":";
+                        mb.playground[d][h][w].solid ? jfile << "true" : jfile << "false";
+                        jfile << ",\"pathLength\":"; 
+                        (mb.playground[d][h][w].pathLength == mb.MAX_INT) ? jfile<<"\"inf\"" 
+                                                                    : jfile<<mb.playground[d][h][w].pathLength;
+                        jfile << ",\"fromPoint\":";
+                        jfile << "[" << mb.playground[d][h][w].fromPoint.x 
+                                  << "," << mb.playground[d][h][w].fromPoint.y 
+                                  << "," << mb.playground[d][h][w].fromPoint.z << "]";
+                        jfile << ",\"visited\":"; 
+                        mb.playground[d][h][w].visited ? jfile << "true" : jfile << "false";
+                        (w < mb.mb_width -1) ? jfile << "}]," : jfile << "}]";
+                    }
+
+                    (h < mb.mb_height -1) ? jfile << " ],"<< std::endl : jfile << " ]" << std::endl;
+                }
+
+               (d < mb.mb_depth -1) ?  jfile << "  ]," << std::endl :  jfile << "  ]" << std::endl;
+            }
+            jfile << "]" << std::endl;
+
+            jfile.close();
+        }
+    }
 };
 
-#include "mazeBotJSON.cpp"
 
 #endif    
